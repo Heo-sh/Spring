50e12af82eb08f29958438d43dd2f0a1559fbfda
Cheon DaYeon <119835857+choendayeon@users.noreply.github.com>
2023-06-22 17:30:45 +0900

    2023-06-22

--- Ex_0621_Thymeleaf/src/main/java/com/korea/thyme/ThymeController.java
+++ Ex_0621_Thymeleaf/src/main/java/com/korea/thyme/ThymeController.java
@@ -1,6 +1,8 @@
 package com.korea.thyme;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,4 +29,116 @@ public class ThymeController {
 		model.addAttribute("vo", vo);
 		return "ex02.html";
 	}
+	
+	@RequestMapping("ex03")
+	public String ex03(Model model) {
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		
+		for (int i = 0; i <= 10; i++) {
+			MemberVO vo = new MemberVO();
+			vo.setMemNo(i);
+			vo.setMemId("user" + i);
+			vo.setMemNm("이름" + i);
+			vo.setRegDt(LocalDateTime.now());
+			
+			list.add(vo);
+		}
+		
+		model.addAttribute("list", list);
+		
+		return "ex03.html";
+	}
+	
+	@RequestMapping("ex04")
+	public String ex04(Model model) {
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		
+		for (int i = 1; i <= 10; i++) {
+			MemberVO vo = new MemberVO();
+			vo.setMemNo(i);
+			vo.setMemId("user" + i);
+			vo.setMemNm("이름" + i);
+			vo.setRegDt(LocalDateTime.now());
+			
+			list.add(vo);
+		}
+		
+		model.addAttribute("list", list);
+		
+		return "ex04.html";
+	}
+	
+	@RequestMapping("ex05")
+	public String ex05(Model model) {
+		List<MemberVO> list = new ArrayList<MemberVO>();
+		
+		for (int i = 1; i <= 10; i++) {
+			MemberVO vo = new MemberVO();
+			vo.setMemNo(i);
+			vo.setMemId("user" + i);
+			vo.setMemNm("이름" + i);
+			vo.setRegDt(LocalDateTime.now());
+			
+			list.add(vo);
+		}
+		model.addAttribute("list", list);
+		
+		return "ex05.html";
+	}
+	
+	@RequestMapping("ex06")
+	public String ex06() {
+		return "ex06.html";
+	}
+
+	@RequestMapping("ex07")
+	public String ex07(Model model, String param1, String param2) {
+		model.addAttribute("param1", param1);
+		model.addAttribute("param2", param2);
+		
+		return "ex07.html";
+	}
+
+	@RequestMapping("ex08")
+	public String ex08(Model model) {
+		MemberVO vo = new MemberVO();
+		vo.setMemNo(1);
+		vo.setMemId("user1");
+		vo.setMemNm("홍길동");
+		vo.setRegDt(LocalDateTime.now());
+		model.addAttribute("vo", vo);
+		return "ex08.html";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
